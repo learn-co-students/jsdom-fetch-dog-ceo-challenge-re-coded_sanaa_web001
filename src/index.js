e76853121f2d3e08e96c723014d028a421a82e3c
Saleem Saleh <salem1234saleem@gmail.com>
2020-07-19 12:55:31 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+fetch(imgUrl)
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
+
+  function renderBooks(images) {
+  const div = document.querySelector('#dog-image-container')
+
+  for (const element of images) {
+      let img = document.createElement('img');
+      img.src = element;
+        div.appendChild(img);
+  }
