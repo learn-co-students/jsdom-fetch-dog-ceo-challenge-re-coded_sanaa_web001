872039cb6843d45a2bd9b381a1a53c68db373d39
Osamah Gharad <osamah.gharred@gmail.com>
2020-08-05 16:40:11 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
-console.log('%c HI', 'color: firebrick')
+
+document.addEventListener("DOMContentLoaded",(event)=>{
+   console.log('%c HI', 'color: firebrick');
+   let dogImageContainer = document.querySelector("#dog-image-container")
+  fetchImages();
+  fetchBreeds();
+});//end of dom
+
+///images fetch
+function fetchImages(){
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+  fetch(imgUrl)
+  .then(response => response.json())
+  .then(json => displayImages(json.message));
+}
+
+function displayImages(images){
+    dogImageContainer.innerHTML += images.map(showImage).join("")
+  }
+  function showImage(image){
+    return `<img style ="display:flex; width:100px height 100px" src =${image}>`
+  }
+
+//end of images fetch
+function fetchBreeds(){
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+  fetch(breedUrl);
+  .then(response => response.json());
+  .then(result => displayBreeds(result.message));
+}
+
+function displayBreeds(breeds){
+   let breedList = document.querySelector("#dog-breeds");
+   breedList.innerHTML+=breeds.map(showBreed).join("");
+
+  }
+}
+function showBreed(breed){
+  return `<li>${breed}</li>`
+}
