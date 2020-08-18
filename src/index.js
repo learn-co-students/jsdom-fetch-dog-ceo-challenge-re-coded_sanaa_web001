03da1ac6d6c3db586ad4c054f0e242864da1815c
Sondos Ahmed <sondosalshami@gmail.com>
2020-08-18 20:52:05 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 console.log('%c HI', 'color: firebrick')
+document.addEventListener("DOMContentLoaded", () => {
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+fetch (imgUrl).then(response => response.json);
+});
+}
