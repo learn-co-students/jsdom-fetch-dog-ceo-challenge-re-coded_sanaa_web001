b12735f0dfbb79d9d86805ad4366cb73467b55bb
Hajar Helal <hajerhelal44@gmail.com>
2020-09-22 11:27:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 function loadImages() {
-  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
-  fetch(imgUrl)
+  const img = "https://dog.ceo/api/breeds/image/random/4"
+  fetch(img)
     .then(res=> res.json())
-    .then(results => {
-      results.message.forEach(image => addImage(image))
+    .then(results => {results.message.forEach(img => addImage(img))
     });
+}
+function addImage(dog) {
+  let cont = document.querySelector('#dog-image-container');
+  cont.appendChild(img);
 }
